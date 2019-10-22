31cd7b16c595140cfcb9b35f0544f7b0f02b166b
Ryohei Ikegami <iofg2100@gmail.com>
2019-10-22 18:43:36 +0900

    Add color legend

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -8,6 +8,35 @@
 #include <tensorflow/lite/kernels/register.h>
 #include <tensorflow/lite/model.h>
 
+namespace {
+
+// From https://github.com/tensorflow/examples/blob/master/lite/examples/image_segmentation/ios/ImageSegmentation/ImageSegmentator.swift
+std::vector<QRgb> legendColorList = {
+    0xFFB300, // Vivid Yellow
+    0x803E75, // Strong Purple
+    0xFF6800, // Vivid Orange
+    0xA6BDD7, // Very Light Blue
+    0xC10020, // Vivid Red
+    0xCEA262, // Grayish Yellow
+    0x817066, // Medium Gray
+    0x007D34, // Vivid Green
+    0xF6768E, // Strong Purplish Pink
+    0x00538A, // Strong Blue
+    0xFF7A5C, // Strong Yellowish Pink
+    0x53377A, // Strong Violet
+    0xFF8E00, // Vivid Orange Yellow
+    0xB32851, // Strong Purplish Red
+    0xF4C800, // Vivid Greenish Yellow
+    0x7F180D, // Strong Reddish Brown
+    0x93AA00, // Vivid Yellowish Green
+    0x593315, // Deep Yellowish Brown
+    0xF13A13, // Vivid Reddish Orange
+    0x232C16, // Dark Olive Green
+    0x00A1C2, // Vivid Blue
+};
+
+} // namespace
+
 MainWindow::MainWindow() {
     // Build UI
     {
