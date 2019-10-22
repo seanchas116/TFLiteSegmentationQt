423a912979e43d5cf9a12bd2bb73b3dbaa8b3dec
Ryohei Ikegami <iofg2100@gmail.com>
2019-10-22 16:37:26 +0900

    Load interpreter

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,9 +1,15 @@
 #include "MainWindow.hpp"
 #include <QApplication>
 #include <tensorflow/lite/interpreter.h>
+#include <tensorflow/lite/kernels/register.h>
 #include <tensorflow/lite/model.h>
 
 MainWindow::MainWindow() {
     auto modelPath = QApplication::applicationDirPath() + "/resources/deeplabv3_257_mv_gpu.tflite";
     auto model = tflite::FlatBufferModel::BuildFromFile(modelPath.toUtf8().data());
+
+    std::unique_ptr<tflite::Interpreter> interpreter;
+
+    tflite::ops::builtin::BuiltinOpResolver resolver;
+    tflite::InterpreterBuilder(*model.get(), resolver)(&interpreter);
 }
